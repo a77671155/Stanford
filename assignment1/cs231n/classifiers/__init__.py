29a2dfed3492d883a1339c089ef93575fadd7c92
Hans <30643465+a77671155@users.noreply.github.com>
2018-06-28 11:18:35 +0800

    Learning record
    Learning record

---
+++ assignment1/cs231n/classifiers/__init__.py
@@ -0,0 +1,2 @@
+from drive.Stanford.CS231n_master.assignment1.cs231n.classifiers.k_nearest_neighbor import *
+from drive.Stanford.CS231n_master.assignment1.cs231n.classifiers.linear_classifier import *
---
+++ assignment1/cs231n/datasets/get_datasets.sh
@@ -0,0 +1,5 @@
+# Get CIFAR10
+wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz 
+mv cifar-10-python.tar.gz drive/Stanford/CS231n_master/assignment1/cs231n/datasets
+tar -xzvf drive/Stanford/CS231n_master/assignment1/cs231n/datasets/cifar-10-python.tar.gz
+rm drive/Stanford/CS231n_master/assignment1/cs231n/datasets/cifar-10-python.tar.gz 
